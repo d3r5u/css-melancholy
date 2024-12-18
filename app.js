cb7ce7fc6a4b8b032ac4c55ae176ef1b1444f13d
dersu137c <181156023+dersu137c@users.noreply.github.com>
2024-12-18 11:06:16 +0500

    Hide go top button when at the top

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // implement sticky header
 window.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector("header");
+  const goTopArrow = document.querySelector("#go-top");
   window.addEventListener("scroll", () => {
-    if(window.scrollY > 100){
+    if (window.scrollY > 100) {
       header.classList.add("sticky");
-    }else if(window.scrollY === 0){
+    } else if (window.scrollY === 0) {
       header.classList.remove("sticky");
+    }
+
+    if (window.scrollY >= 500) {
+      goTopArrow.classList.remove("hidden");
+    } else if (window.scrollY < 500) {
+      goTopArrow.classList.add("hidden");
     }
   });
 
   // clear textarea
   const textarea = document.querySelector("textarea");
-  textarea.innerText = '';
-})
-
+  textarea.innerText = "";
+});
